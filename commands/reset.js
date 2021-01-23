ca170505ca4417497ee3bbae79ba5db44bda5ead
lawlietl4 <nroberson7712@gmail.com>
2021-01-23 13:19:22 -0500

    added updated reset.js

--- commands/reset.js
+++ commands/reset.js
@@ -2,7 +2,12 @@ module.exports = {
     name: "reset",
     description: "resets the bot",
     guildOnly: true,
-    execute(message, args){
-        
+    execute(message,args) {
+        if(message.author.id !== '196436703752355840'){
+            message.channel.send("You are not allowed to use this command");
+        }
+	console.log("Bot reset!");
+        message.channel.send("bot resetting");
+        process.exit(0);
     }
 };
