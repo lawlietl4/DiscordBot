01b88f8abcd3f354e13dcdb3c33b40834f113115
Nicholas Roberson <60959101+lawlietl4@users.noreply.github.com>
2021-08-02 19:45:14 -0600

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,7 +1,7 @@
-module.exports = {
-    name: 'userinfo',
-    description: 'user info sheet',
-    execute(message, args){
+module.exports = { //following module exportation rules
+    name: 'userinfo', //give module name
+    description: 'user info sheet', //give module description
+    execute(message, args){ //this is where the bot retrieves the important info
         return message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
     },
 };
